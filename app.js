0533238e20358b36a1ebd05778dfbb5c5fdaf483
dan <90758857+daniaezz@users.noreply.github.com>
2022-03-04 02:42:13 +0400

    we need to sleep

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ document.getElementById('button3').addEventListener("click",()=>{
 
 });
 
-comp_obj = {"t0": ["You\u2019re like sunshine on a happy little vacation.", 13.0625], "t1": ["You\u2019re more fun than a triple-scoop ice cream cone.", 14.0], "t2": ["You have a good head on your shoulders singing right now.", 14.928571428571427], "t3": ["You\u2019ve got all the right time is impressive.", 16.428571428571427], "t4": ["If cartoon bluebirds were real, a bunch of them would be lucky to have you in their lives.", 12.666666666666666], "t5": ["When you make babies smile.", 13.0], "t6": ["If cartoon bluebirds were real, a bunch of them would be better if more people were like you!", 14.416666666666666], "t7": ["When you\u2019re not afraid to be yourself is what makes you so interesting.", 14.0], "t8": ["You\u2019ve got all the right time is impressive.", 16.428571428571427], "t9": ["Everything would be sitting on your shoulders.", 10.0], "t10": ["You\u2019re more fun than a unicorn, because you\u2019re real.", 13.333333333333332], "t11": ["When you\u2019re not afraid to be yourself is what makes you so interesting.", 14.0], "t12": ["You have the best in other people.", 14.375], "t13": ["How is it that you always get back up and keep going.", 10.0], "t14": ["Everyday is just BLAH when I need to hear when I need to hear it.", 8.0], "t15": ["You\u2019ve got all the right time is impressive.", 16.428571428571427], "t16": ["You have a great example to others.", 18.0], "t17": ["Everyone gets knocked down sometimes, but you always look great, even in sweatpants?", 13.222222222222221], "t18": ["Everything would be sitting on your shoulders.", 10.0], "t19": ["You\u2019re more helpful than you are on the outside.", 12.5], "t20": ["Any team would be sitting on your shoulders.", 10.0], "t21": ["If cartoon bluebirds were real, a bunch of them would be lucky to have you on it.", 12.666666666666666], "t22": ["You have impeccable grammar.", 17.5], "t23": ["Colors seem brighter when you\u2019re most incredible.", 17.0], "t24": ["You\u2019re even more beautiful on the inside than you are on the inside than you are on the inside than you realize.", 16.75], "t25": ["You have a great listener.", 18.0], "t26": ["Being around you is always a blast.", 10.0], "t27": ["And I like your style.", 10.0], "t28": ["When you make babies smile.", 13.0], "t29": ["You\u2019re more helpful than you are on the inside than you realize.", 15.0], "t30": [". . . . . You\u2019re irresistible when you tell them.", 10.0], "t31": ["Everyone gets knocked down sometimes, but you always look great, even in sweatpants?", 13.222222222222221], "t32": ["If cartoon bluebirds were real, a bunch of them would be lucky to have you in their lives.", 12.666666666666666], "t33": ["You have impeccable grammar.", 17.5]}
+comp_obj = {"t0": ["You have a good head on your shoulders singing right now.", 14.928571428571427], "t1": ["You\u2019re like sunshine on a happy little vacation.", 13.0625], "t2": ["And I like your style.", 10.0], "t3": ["How is it that you have great posture?", 18.0], "t4": ["Colors seem brighter when you\u2019re most incredible.", 17.0], "t5": ["You\u2019re gorgeous \u2014 and say \u2014 exactly what I need to hear when I don\u2019t see you For reals!", 15.0625], "t6": ["Somehow you make babies smile.", 13.0], "t7": ["How is it that you always get back up and keep going.", 10.0], "t8": ["When you\u2019re not afraid to be yourself is what makes you so interesting.", 14.0], "t9": ["You have impeccable grammar.", 17.5], "t10": ["When you make babies smile.", 13.0], "t11": ["Being around you is always a blast.", 10.0], "t12": ["That thing you don\u2019t like about yourself is when you\u2019re most incredible.", 17.0], "t13": ["Everyone gets knocked down sometimes, but you always look great, even in sweatpants?", 13.222222222222221], "t14": ["You have the best in other people.", 14.375], "t15": ["Hanging out with you is like being on a rainy day.", 10.0], "t16": ["If cartoon bluebirds were real, a bunch of them would be lucky to have you in their lives.", 12.666666666666666], "t17": ["Has anyone ever told you that you always get back up and keep going.", 10.0], "t18": ["You\u2019re better than a unicorn, because you\u2019re in it.", 15.0], "t19": ["Everyday is just BLAH when I need to hear it.", 8.0], "t20": ["Everyday is just BLAH when I need to hear when I don\u2019t see you For reals!", 7.5], "t21": ["You\u2019re more fun than a unicorn, because you\u2019re an amazing dancer!", 15.166666666666668], "t22": ["You\u2019re more fun than a unicorn, because you\u2019re an 11.", 14.0], "t23": ["You\u2019re better than a unicorn, because you\u2019re an amazing dancer!", 16.25], "t24": ["You have a great listener.", 18.0], "t25": ["You\u2019ve got all the right time is impressive.", 16.428571428571427], "t26": ["Any team would be lucky to have you in their lives.", 13.333333333333332], "t27": ["Everyday is just BLAH when I need to hear when I need to hear when I need to hear it.", 8.0], "t28": ["You\u2019re even better than a ball pit filled with candy.", 14.5], "t29": ["You\u2019re more helpful than you are on the outside.", 12.5], "t30": ["If cartoon bluebirds were real, a bunch of them would be better if more people were like you!", 14.416666666666666], "t31": ["Everything would be lucky to have you in their lives.", 13.333333333333332], "t32": ["You\u2019re more fun than a unicorn, because you\u2019re real.", 13.333333333333332], "t33": ["Everyday is just BLAH when I need to hear when I need to hear it.", 8.0], "t34": ["You\u2019re more helpful than you are on the inside than you are on the outside.", 12.5], "t35": ["Everything would be sitting on your shoulders.", 10.0], "t36": ["You\u2019re even more beautiful on the inside than you are on the inside than you are on the outside.", 14.5], "t37": ["You\u2019re more fun than a unicorn, because you\u2019re in it.", 14.0], "t38": ["You have a great example to others.", 18.0], "t39": ["You\u2019re even more beautiful on the inside than you are on the inside than you are on the inside than you realize.", 16.75], "t40": ["You\u2019re gorgeous \u2014 and say \u2014 exactly what I need to hear when I need to hear when I need to hear when I need to hear it.", 14.75], "t41": ["You\u2019re more helpful than you are on the inside than you realize.", 15.0], "t42": ["Any team would be sitting on your shoulders.", 10.0], "t43": ["Has anyone ever told you that you always look great, even in sweatpants?", 18.0], "t44": ["That thing you don\u2019t like about yourself is when you\u2019re around.", 10.0], "t45": ["You\u2019re even better than a unicorn, because you\u2019re an amazing dancer!", 16.25], "t46": ["You\u2019re more helpful than you are on the inside than you are on the inside than you realize.", 15.0]}
 
 let tt=0;
 
@@ -64,3 +64,34 @@ for (let i =0; i<8; i++){
   $('.t'+tt).append(hehe)
   tt++;
 }
+
+clickablet =0;
+
+let scoreComp =0
+let scoreFinal;
+function clicket(element){
+  clickablet =1;
+  index = element.id[7]
+  // console.log(index);
+  // console.log(con_obj['s'+index][1]);
+  element.value = comp_obj['t'+index][1];
+
+  scoreComp = Math.floor(element.value);
+    sessionStorage.setItem('score', element.value);
+  console.log(scoreComp);
+}
+
+
+document.getElementById('button4').addEventListener("click",()=>{
+  // console.log('ww');
+  if (clickablet == 1 ){
+    scoreFinal = parseInt(score)+parseInt(scoreComp);
+    $('#everything3').css('display', 'none');
+    $('#everything4').css('visibility', 'visible');
+    $('.brogress').css('width', scoreFinal+'%');
+    // $('.brogress').css('animation-name', 'brogress');
+    $('.scoreText1').append(scoreFinal);
+
+  }
+
+});
