92af8ca5f3e612a09472c8d5f35e265cb85d620b
dan <90758857+daniaezz@users.noreply.github.com>
2022-03-03 16:36:54 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+data = fetch('jj.json')
+  .then(response => {
+     return response.json();
+  })
+  .then(jsondata => console.log(data));
+// let smth = jj;
+// let newHTML = `<div>${recipe."Dear GPTessa, I have to confess that I like you. You are the true beauty of the world. To put it in old words, you are the one who taught me the art of the sword. You are the one who taught me how to walk, talk, and talk to people."}</div>`;
+//
+// document.getElementById('con-text').insertAdjacentHTML('beforeend', newHTML)
