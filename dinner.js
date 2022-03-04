f693caf85045ec0d75d9e5a7312dca5af8ca04db
dan <90758857+daniaezz@users.noreply.github.com>
2022-03-05 00:48:41 +0400

    menu done for now sticky added and formating for meal name whatever that means

--- dinner.js
+++ dinner.js
@@ -3,8 +3,12 @@ var vv = 0;
 for (let i =0; i<3; i++){
   rand_con = Math.floor(Math.random()*20);
   hehe = starters_obj['v'+rand_con][0];
+  for (let j = 0; j<hehe.length; j++){
+    console.log(hehe[j]);
+    $('.v'+vv).append(hehe[j]+" ")
+  }
   console.log(hehe);
-  $('.v'+vv).append(hehe)
+
   vv++;
 }
 
@@ -14,7 +18,10 @@ for (let i =0; i<3; i++){
   rand_con = Math.floor(Math.random()*20);
   hehe = mains_obj['w'+rand_con][0];
   console.log(hehe);
-  $('.w'+ww).append(hehe)
+  for (let j = 0; j<hehe.length; j++){
+    console.log(hehe[j]);
+    $('.w'+ww).append(hehe[j]+" ")
+  }
   ww++;
 }
 
@@ -24,6 +31,10 @@ for (let i =0; i<3; i++){
   rand_con = Math.floor(Math.random()*20);
   hehe = desserts_obj['x'+rand_con][0];
   console.log(hehe);
-  $('.x'+xx).append(hehe)
+  for (let j = 0; j<hehe.length; j++){
+    console.log(hehe[j]);
+    $('.x'+xx).append(hehe[j]+" ")
+  }
+  // $('.x'+xx).append(hehe)
   xx++;
 }
